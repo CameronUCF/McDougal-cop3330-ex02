5388cb135284b003b45855eaf4f6000708f2d1d6
Geetis <capiix1@gmail.com>
2021-09-13 03:14:38 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Cameron McDougal
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is the input string?");
+        String input = scanner.nextLine();
+
+        scanner.close();
+
+        // Challenge 1
+        if(input.length() == 0)
+            System.out.println("Please enter a valid string.");
+        else
+            System.out.println(input + " has " + input.length() + " characters.");
+    }
+}
